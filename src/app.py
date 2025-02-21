446abbd2359c1f1490faa2daf62529fdb2bfbd01
luciamam <lumanmba2023@gmail.com>
2025-02-21 09:50:34 +0100

    Iniciando el proyecto , configurando la  ruta inicial y la ruta registrarse

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from  flask import Flask, render_template, url_for, flash
+
+
+app=Flask(__name__)
+
+
+
+
+
+@app.route('/')
+def inicio():
+    return " soy la ruta raiz  "
+
+
+
+
+@app.route('/registrarse', methods=['GET','POST'])
+def registro():
+    return " soy la ruta registro "
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
